79fbd0e87f783241637d28cae7e9f404f9cecb23
rhday <ruariday@gmail.com>
2020-03-18 12:32:04 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,15 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand)) # this means that if the array called BRANDS does not include 
+                                  #the @brand passed already then put that brand in the array.
+                                  #otherwise discard the item. Essentially weeding out any duplicates.
+    BRANDS << @brand
+    end 
   end
 
   def cobble
